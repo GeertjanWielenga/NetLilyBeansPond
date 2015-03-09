890f1eeaef66010e8686ad083ff6a99d3bf1c9f2
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-03-09 07:02:49 +0100

    lilypond for NetBeans

--- NetLilyBeansPond/NetLilyBeansPond/src/main/java/org/netbeans/nlbp/package-info.java
+++ NetLilyBeansPond/NetLilyBeansPond/src/main/java/org/netbeans/nlbp/package-info.java
@@ -1,9 +1,11 @@
 @TemplateRegistrations({
+    //Origin: http://tex.blogoverflow.com/2011/11/sheet-music-with-lilypond-and-frescobaldi/
     @TemplateRegistration(
             requireProject = false,
             folder = "LilyPond",
             displayName = "Happy Birthday, Java!",
             content = "birthday.ly"),
+    //Origin: http://en.wikipedia.org/wiki/LilyPond
     @TemplateRegistration(
             requireProject = false,
             folder = "LilyPond",
